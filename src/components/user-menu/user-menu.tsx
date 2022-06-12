653c959486abb23bb7504e47848d8127b96c3c54
Haru-Yuki <dimakred@gmail.com>
2022-06-12 22:48:03 +0300

    Added menu

--- src/components/user-menu/user-menu.tsx
+++ src/components/user-menu/user-menu.tsx
@@ -2,17 +2,23 @@ import React, {useEffect, useState} from "react";
 import axios from "axios";
 
 import './user-menu.scss';
+import {useForm} from "react-hook-form";
 
 export let currency: Array<any>;
 
 const UserMenu = () => {
+    const { register, handleSubmit } = useForm<any>();
+
     const user = JSON.parse(localStorage.getItem('mono-user'));
     const [balance, setBalance] = useState(0);
     const [creditLim, setCreditLim] = useState(0);
     const [topCurrency, setTopCurrency] = useState([]);
     const [isShowCurrency, setIsShowCurrency] = useState(false);
     const [isShowPersonalSettings, setIsShowPersonalSettings] = useState(false);
+
     const [showAccountMenu, setShowAccountMenu] = useState(false);
+    const [showAnalMenu, setShowAnalMenu] = useState(false);
+    const [showFundsMenu, setShowFundsMenu] = useState(false);
 
     const currencyCodes = [{
             code: 980,
@@ -65,13 +71,13 @@ const UserMenu = () => {
         return <div className="user-menu-wrapper">
             <div>{`Вітаємо, ${user.name}`}</div>
             <div>{`Загальний баланс: ${balance.toFixed(2)} грн.`}</div>
-            <div>{`Заборгованість по кредитному ліміту: ${creditLim.toFixed(2)} грн.`}</div>
+            {parseInt(creditLim.toFixed(2), 10) > 0 ? <div>{`Заборгованість по кредитному ліміту: ${creditLim.toFixed(2)} грн.`}</div>: null}
             <ol>
                 <li>
                     <a href="#" onClick={() => setShowAccountMenu(true)}>Перевірити баланс за рахунками</a>
                 </li>
                 <li>
-                    <a>Перейти в меню аналізу виписки</a>
+                    <a href="#" onClick={() => setShowAnalMenu(true)}>Перейти в меню аналізу виписки</a>
                 </li>
                 <li>
                     <a href="#" onClick={() => setIsShowCurrency(!isShowCurrency)}>Отримати курс валют</a>
@@ -132,7 +138,7 @@ const UserMenu = () => {
         </div>
     };
 
-    const renderAccountMenu = () => {
+    const renderAccountMenu = (menu: any) => {
         return <div className="user-menu-wrapper user-menu-wrapper--account">
             {user.accounts.map((account: any) =>
                 <div key={account.id}>
@@ -143,19 +149,93 @@ const UserMenu = () => {
                     </div>
                 </div>
             )}
+            {menu}
             <a href="#" onClick={() => setShowAccountMenu(false)}>Повернутись до головного екрану</a>
         </div>
     };
 
-    if (!showAccountMenu) {
+    const renderAnalMenu = () => {
+        return <div className="user-menu-wrapper user-menu-wrapper--account">
+            <ol>
+                <li>
+                    <a onClick={() => {
+                        setShowFundsMenu(true)
+                        setShowAnalMenu(false)
+                    }} href="#">Перейти до меню підрахунку</a>
+                </li>
+                <li>
+                    <a href="#">Порівняти загальну суму витрат</a>
+                </li>
+                <li>
+                    <a href="#">Порівняти загальну суму поповнень</a>
+                </li>
+                <li>
+                    <a href="#">Порівняти загальний оборот коштів</a>
+                </li>
+                <li>
+                    <a href="#">Аналізувати витрати за категоріями</a>
+                </li>
+                <li>
+                    <a href="#" onClick={() => setShowAnalMenu(false)}>Повернутись до головного екрану</a>
+                </li>
+            </ol>
+        </div>
+    };
+
+    const renderFundsMenu = () => {
+        const menu = <>
+            <form onSubmit={handleSubmit((data) => prepareDataForAnal(data, user.accounts))}>
+                <input style={{width: '250px'}} type="text" placeholder="Введіть IBAN для підрахунку" {...register("iban")} />
+                <select {...register("type")}>
+                    <option value="refills">Підрахувати загальну суму витрат</option>
+                    <option value="writeoff">Підрахувати загальну суму поповнень</option>
+                    <option value="moneycircle">Підрахувати загальний оборот коштів</option>
+                    <option value="extract">Показати виписку</option>
+                </select>
+                <label>
+                    Початкова дата
+                    <input type="date" {...register("dateFrom")} />
+                </label>
+                <label>
+                    Кінцева дата
+                    <input type="date" {...register("dateTo")} />
+                </label>
+                <button type="submit">Підрахувати</button>
+            </form>
+        </>
+
+        return <>
+            {renderAccountMenu(menu)}
+        </>
+    };
+
+    if (!showAccountMenu && !showAnalMenu && !showFundsMenu) {
         return renderMainMenu();
     }
 
     if (showAccountMenu) {
-        return renderAccountMenu();
+        return renderAccountMenu(null);
     }
 
+    if (showAnalMenu) {
+        return renderAnalMenu();
+    }
+
+    if (showFundsMenu) {
+        return renderFundsMenu();
+    }
 };
+
+const prepareDataForAnal = (data: any, accounts: Array<any>) => {
+    const id = accounts.find(account => account.iban === data.iban).id;
+
+    console.log({
+        id: id,
+        type: data.type,
+        dateFrom: data.dateFrom,
+        dateTo: data.dateTo
+    });
+}
 
 const getTotalBalance = async (currency: any, clientId: string) => {
     return await axios.post(`https://financeassistantdiplom.herokuapp.com/account/totalbalance?clientId=${clientId}`, currency);
